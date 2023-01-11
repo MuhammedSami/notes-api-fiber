cc3150297efade171c553795d854b58ce7c5fc87
MuhammedSami <muhammedsami6@gmail.com>
2023-01-11 19:22:23 +0300

    feat: add notes api resource

--- main.go
+++ main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-
-	"github.com/MuhammedSami/notes-api-fiber/database"
 	"github.com/gofiber/fiber/v2"
+	"github.com/MuhammedSami/notes-api-fiber/database"
+	"github.com/MuhammedSami/notes-api-fiber/router"
 )
 
 func main() {
@@ -11,11 +11,9 @@ func main() {
 	
 	// Connect to the Database
 	database.ConnectDB()
-	// Send a string back for GET calls to the endpoint "/"
-    app.Get("/", func(c *fiber.Ctx) error {
-        err := c.SendString("And the API is UP!")
-        return err
-    })
+	
+	// Setup the router
+	router.SetupRoutes(app)
 	app.Listen(":3000")
 	
 }
